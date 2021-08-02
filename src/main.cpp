a2fc7e7210ddbea57b2c2ca6124145eced34fb77
DanRivaille <dan.santos.hd@outlook.com>
2021-08-01 20:15:07 -0400

    Se agrega el codiga para crear una ventana

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,45 @@
-#include <SDL2/SDL.h>
+#include <cstdlib>
 #include <stdio.h>
+#include <SDL2/SDL.h>
+
+// Screen dimensions
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char ** argv)
 {
-  printf("It's all ok\n");
+  SDL_Window* window = NULL;
+  SDL_Surface* screen_surface = NULL;
 
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    printf("SDL could not initialize, SDL_Error: %s\n", SDL_GetError());
+    exit(EXIT_FAILURE);
+  }
+  else
+  {
+    window = SDL_CreateWindow("Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-  return 0;
+    if (NULL == window)
+    {
+      printf("Window could not be created, SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+      screen_surface = SDL_GetWindowSurface(window);
+
+      SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+
+      SDL_UpdateWindowSurface(window);
+
+      SDL_Delay(2000);
+
+      SDL_DestroyWindow(window);
+    }
+
+    SDL_Quit();
+  }
+
+  return EXIT_SUCCESS;
 }
 
