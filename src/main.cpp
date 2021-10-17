184cd699dfa706c254bb685d030fcbfd292b7556
DanRivaille <dan.santos.hd@outlook.com>
2021-10-17 20:33:52 -0300

    Se modulariza la inicializacion de recursos

--- src/main.cpp
+++ src/main.cpp
@@ -7,48 +7,65 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 // Prototypes
-void close(SDL_Window *window, SDL_Renderer *renderer);
+bool initResources(SDL_Window *window, SDL_Renderer *renderer);
 void mainLoop(SDL_Renderer *renderer);
+void close(SDL_Window *window, SDL_Renderer *renderer);
 
 int main(int argc, char ** argv)
 {
   SDL_Window* window = NULL;
   SDL_Renderer *renderer;
 
-  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  if (initResources(window, renderer))
   {
-    printf("SDL could not initialize, SDL_Error: %s\n", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
-  else
-  {
-    // Creation of window with some parameters
-    window = SDL_CreateWindow("Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
-    if (NULL == window)
-    {
-      printf("Window could not be created, SDL_Error: %s\n", SDL_GetError());
-    }
-    else
-    {
-      // Get the renderer for window
-      renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-      if (NULL == renderer)
-      {
-        printf("Renderer could not be created! - SDL Error: %s\n", SDL_GetError());
-      }
-      else
-      {
-        mainLoop(renderer);
-      }
-    }
+    mainLoop(renderer);
 
     close(window, renderer);
   }
+  else
+  {
+    exit(EXIT_FAILURE);
+  }
+
 
   return EXIT_SUCCESS;
 }
 
+/**
+ * Inicializa los recursos y valida su correcta inicializacion
+ * @return true si todo se pudo inicializar bien, en caso contrario false
+ * */
+bool initResources(SDL_Window *window, SDL_Renderer *renderer)
+{
+  // Initialize the resources from SDL2
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    printf("SDL could not initialize, SDL_Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  // Creation of window with some parameters
+  window = SDL_CreateWindow("Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (NULL == window)
+  {
+    printf("Window could not be created, SDL_Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  // Get the renderer for window
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (NULL == renderer)
+  {
+    printf("Renderer could not be created! - SDL Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Bucle principal del juego
+ * */
 void mainLoop(SDL_Renderer *renderer)
 {
   SDL_Event event;
