19c97934e87649ddcf68b5f3810bf836179e5cec
DanRivaille <dan.santos.hd@outlook.com>
2021-10-17 20:57:38 -0300

    Se arregla un bug con los punteros del renderer

--- src/main.cpp
+++ src/main.cpp
@@ -7,60 +7,27 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 // Prototypes
-bool initResources(SDL_Window *window, SDL_Renderer *renderer);
+bool initResources(SDL_Window **window, SDL_Renderer **renderer);
 void mainLoop(SDL_Renderer *renderer);
-void close(SDL_Window *window, SDL_Renderer *renderer);
+void close(SDL_Window **window, SDL_Renderer **renderer);
 
 int main(int argc, char ** argv)
 {
   SDL_Window* window = NULL;
-  SDL_Renderer *renderer;
+  SDL_Renderer *renderer = NULL;
 
-  if (initResources(window, renderer))
+  if (initResources(&window, &renderer))
   {
     mainLoop(renderer);
 
-    close(window, renderer);
+    close(&window, &renderer);
   }
   else
   {
     exit(EXIT_FAILURE);
   }
 
-
   return EXIT_SUCCESS;
-}
-
-/**
- * Inicializa los recursos y valida su correcta inicializacion
- * @return true si todo se pudo inicializar bien, en caso contrario false
- * */
-bool initResources(SDL_Window *window, SDL_Renderer *renderer)
-{
-  // Initialize the resources from SDL2
-  if (SDL_Init(SDL_INIT_VIDEO) < 0)
-  {
-    printf("SDL could not initialize, SDL_Error: %s\n", SDL_GetError());
-    return false;
-  }
-
-  // Creation of window with some parameters
-  window = SDL_CreateWindow("Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-  if (NULL == window)
-  {
-    printf("Window could not be created, SDL_Error: %s\n", SDL_GetError());
-    return false;
-  }
-
-  // Get the renderer for window
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  if (NULL == renderer)
-  {
-    printf("Renderer could not be created! - SDL Error: %s\n", SDL_GetError());
-    return false;
-  }
-
-  return true;
 }
 
 /**
@@ -80,10 +47,13 @@ void mainLoop(SDL_Renderer *renderer)
       if (SDL_QUIT == event.type)
         quit = true;
     }
-    
     // Clear screen
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
+
+    // Draw things
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawPoint(renderer, 100, 100);
 
     // Update Screen
     SDL_RenderPresent(renderer);
@@ -91,20 +61,52 @@ void mainLoop(SDL_Renderer *renderer)
 }
 
 /**
- * Close all resources, check if window and renderer aren't NULL
+ * Inicializa los recursos y valida su correcta inicializacion
+ * @return true si todo se pudo inicializar bien, en caso contrario false
  * */
-void close(SDL_Window *window, SDL_Renderer *renderer)
+bool initResources(SDL_Window **window, SDL_Renderer **renderer)
 {
-  if (renderer != NULL)
+  // Initialize the resources from SDL2
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
   {
-    SDL_DestroyRenderer(renderer);
-    renderer = NULL;
+    printf("SDL could not initialize, SDL_Error: %s\n", SDL_GetError());
+    return false;
   }
 
-  if (window != NULL)
+  // Creation of window with some parameters
+  *window = SDL_CreateWindow("Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (NULL == *window)
   {
-    SDL_DestroyWindow(window);
-    window = NULL;
+    printf("Window could not be created, SDL_Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  // Get the renderer for window
+  *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
+  if (NULL == *renderer)
+  {
+    printf("Renderer could not be created! - SDL Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Close all resources, check if window and renderer aren't NULL
+ * */
+void close(SDL_Window **window, SDL_Renderer **renderer)
+{
+  if (*renderer != NULL)
+  {
+    SDL_DestroyRenderer(*renderer);
+    *renderer = NULL;
+  }
+
+  if (*window != NULL)
+  {
+    SDL_DestroyWindow(*window);
+    *window = NULL;
   }
 
   SDL_Quit();
